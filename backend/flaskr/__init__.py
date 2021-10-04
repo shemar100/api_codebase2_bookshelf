209ab428023999eec68bd74f90856f9df28cdb0e
shemar100 <anderson.shemar17@gmail.com>
2021-10-04 06:06:36 -0500

    added not allowed error handler

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -144,11 +144,19 @@ def create_app(test_config=None):
         }), 422
 
     @app.errorhandler(400)
-    def unprocessable(error):
+    def bad_request(error):
         return jsonify({
         "success": False, 
         "error": 400,
         "message": "bad request"
         }), 400
+
+    @app.errorhandler(405)
+    def not_allowed(error):
+        return jsonify({
+        "success": False, 
+        "error": 405,
+        "message": "method not allowed"
+        }), 405
 
     return app
