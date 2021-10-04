ff150df2a200e2338c235d29e8eaf7256e4101ef
shemar100 <anderson.shemar17@gmail.com>
2021-10-04 05:49:00 -0500

    error handler implementation

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -126,4 +126,14 @@ def create_app(test_config=None):
             })
         except:
             abort(422)
+    
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False, 
+            "error": 404,
+            "message": "Not found"
+            }), 404
+
+
     return app
